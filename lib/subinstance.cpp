0e87039d194924f1afc3090aa9333e6ba2c3c2e3
Florian Fontan <dev@florian-fontan.fr>
2018-05-10 15:06:21 +0300

    Implement SubInstance class

---
+++ lib/subinstance.cpp
@@ -0,0 +1,105 @@
+#include "subinstance.hpp"
+#include "solution.hpp"
+
+SubInstance::SubInstance(const Instance& ins): instance_(ins)
+{
+    reduced_solution_ = new Solution(ins);
+}
+
+SubInstance& SubInstance::operator=(const SubInstance sub)
+{
+    if (this != &sub) {
+        *reduced_solution_ = *sub.reduced_solution_;
+    }
+    return *this;
+}
+
+SubInstance::~SubInstance()
+{
+    delete reduced_solution();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool SubInstance::check_opt(Profit v) const
+{
+    if (instance().optimal_solution() != NULL
+            && !sol_red_opt_
+            && v != instance().optimum()) {
+        std::cout << "V " << v << " != OPT " << instance().optimum() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool SubInstance::check_sopt(const Solution& sol) const
+{
+    if (!sol.feasible()) {
+        std::cout << "NOT FEASIBLE" << std::endl;
+        return false;
+    }
+    if (instance().optimal_solution() != NULL
+                && !sol_red_opt_
+                && sol.profit() != instance().optimum()) {
+        std::cout << "V " << sol.profit() << " != OPT " << instance().optimum() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool SubInstance::check_ub(Profit p) const
+{
+    if (instance().optimal_solution() != NULL
+            && !sol_red_opt_
+            && p < instance().optimum()) {
+        std::cout << "U " << p << " < OPT " << instance().optimum() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool SubInstance::check_lb(Profit v) const
+{
+    if (instance().optimal_solution() != NULL
+            && !sol_red_opt_
+            && v > instance().optimum()) {
+        std::cout << "V " << v << " > OPT " << instance().optimum() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool SubInstance::check_sol(const Solution& sol) const
+{
+    if (!sol.feasible()) {
+        std::cout << "NOT FEASIBLE" << std::endl;
+        return false;
+    }
+    if (instance().optimal_solution() != NULL
+            && !sol_red_opt_
+            && sol.profit() > instance().optimum()) {
+        std::cout << "V " << sol.profit() << " > OPT " << instance().optimum() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+std::ostream& operator<<(std::ostream& os, const SubInstance& sub)
+{
+    const Instance& ins = sub.instance();
+    os <<  "N " << ins.item_number();
+    os << " C";
+    for (AgentIdx i=0; i<ins.agent_number(); ++i)
+        os << " " << sub.capacity(i);
+    os << std::endl;
+
+    for (ItemPos j=0; j<ins.item_number(); ++j) {
+        os << j << ":" << std::flush;
+        for (AltIdx k: ins.item(j).alt)
+            os << "; " << ins.alternative(k);
+        if (ins.optimal_solution() != NULL)
+            os << " O " << ins.optimal_solution()->agent(j);
+        os << std::endl;
+    }
+    return os;
+}
---
+++ lib/subinstance.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "instance.hpp"
+#include "solution.hpp"
+
+class SubInstance
+{
+
+public:
+
+    SubInstance(const Instance& ins);
+    SubInstance& operator=(const SubInstance sub);
+
+    ~SubInstance();
+
+    inline const Instance& instance() const { return instance_; }
+
+    inline ItemIdx job_number() const { return instance().item_number()-reduced_solution()->item_number(); }
+    inline Weight capacity(AgentIdx i) const { return reduced_solution()->remaining_capacity(i); }
+    inline bool feasible() const { return feasible_; }
+
+    /**
+     * Apply variable reduction.
+     */
+    void reduce(const Solution& sol, bool verbose = false);
+    Solution* reduced_solution() const { return reduced_solution_; };
+    inline int reduced(ItemIdx j) const { return reduced_solution()->agent(j); }
+    void set(ItemIdx j, int b);
+
+    bool check_opt(Profit p) const;
+    bool check_sopt(const Solution& sol) const;
+    bool check_ub(Profit p) const;
+    bool check_lb(Profit p) const;
+    bool check_sol(const Solution& sol) const;
+
+private:
+
+    bool update_deadlines(const std::vector<ItemIdx>& scheduled_jobs);
+
+    const Instance& instance_;
+
+    Solution* reduced_solution_;
+
+    bool sol_red_opt_ = false;
+    bool feasible_ = true;
+};
+
+std::ostream& operator<<(std::ostream& os, const SubInstance& sub);
+
