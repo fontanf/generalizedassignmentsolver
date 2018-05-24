65c1dbc95d643d91d68e7fc4aded7e4e6dbb143a
Florian Fontan <dev@florian-fontan.fr>
2018-05-24 09:22:37 +0300

    Add tests for LR

---
+++ ub_lagrangian/test.cpp
@@ -0,0 +1,46 @@
+#include "gap/lib/tester.hpp"
+#include "gap/lib/generator.hpp"
+#include "gap/ub_lagrangian/lagrangian.hpp"
+#include "gap/opt_cplex/cplex.hpp"
+
+using namespace gap;
+
+Profit opt_cplex_test(Instance &ins)
+{
+    return sopt_cplex(ins, NULL).profit();
+}
+
+Profit ub_lagrangian_test(Instance& ins)
+{
+    return ub_lagrangian(ins).bound;
+}
+
+std::vector<Profit (*)(Instance&)> tested_functions()
+{
+    return {
+        opt_cplex_test,
+        ub_lagrangian_test,
+    };
+}
+
+TEST(LR, DataPisingerSmall)
+{
+    test_gen(
+        {"c", "d"},
+        {2, 3},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+        {0,1,2},
+        1,
+        {tested_functions()}, 1);
+}
+
+TEST(LR, DataPisingerMedium)
+{
+    test_gen(
+        {"c", "d"},
+        {5, 10},
+        {10, 20, 30, 40},
+        {0,1,2},
+        1,
+        {tested_functions()}, 1);
+}
