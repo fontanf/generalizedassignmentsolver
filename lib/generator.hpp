dcab1d9a2b3b7912b6111ceb3085a4864b0785db
Florian Fontan <dev@florian-fontan.fr>
2018-06-07 15:56:00 +0300

    Update Generator

--- lib/generator.hpp
+++ lib/generator.hpp
@@ -9,35 +9,43 @@ Instance generate(std::string type, AgentIdx m, ItemIdx n, int obj, int seed);
 
 struct GenParams
 {
-    AgentIdx m = 5;
-    ItemIdx n = 25;
-    Value r = 100;
-    int bp = 0;
-    int bw = 0;
-    int ep = 0;
-    int ew = 0;
-    int h = 100;
-    int hmax = 100;
-    int seed = 1;
-
+    AgentIdx m;
+    ItemIdx n;
+    Value r;
+    int bp;
+    int bw;
+    int ep;
+    int ew;
+    int h;
+    int hmax;
+    int seed;
 };
 
 std::ostream& operator<<(std::ostream &os, const GenParams& p);
 
-const GenParams P0W0E0{5, 25, 100, 0,  0, 0, 0, 75, 100, 1};
-const GenParams P0W1E0{5, 25, 100, 0,  1, 0, 0, 75, 100, 1};
-const GenParams P0W2E0{5, 25, 100, 0, -1, 0, 0, 75, 100, 1};
-const GenParams P1W0E0{5, 25, 100, 1,  0, 0, 0, 75, 100, 1};
-const GenParams P1W1E0{5, 25, 100, 1,  1, 0, 0, 75, 100, 1};
-const GenParams P1W2E0{5, 25, 100, 1, -1, 0, 0, 75, 100, 1};
-const GenParams P0W0E1{5, 25, 100, 0,  0, 1, 1, 75, 100, 1};
-const GenParams P0W1E1{5, 25, 100, 0,  1, 1, 1, 75, 100, 1};
-const GenParams P0W2E1{5, 25, 100, 0, -1, 1, 1, 75, 100, 1};
-const GenParams P1W0E1{5, 25, 100, 1,  0, 1, 1, 75, 100, 1};
-const GenParams P1W1E1{5, 25, 100, 1,  1, 1, 1, 75, 100, 1};
-const GenParams P1W2E1{5, 25, 100, 1, -1, 1, 1, 75, 100, 1};
-
 Instance generate(const GenParams& p);
+
+class GenParamsFactory
+{
+
+public:
+
+    const GenParams P0W0E0{5, 25, 100, 0,  0, 0, 0, 75, 100, 1};
+    const GenParams P0W1E0{5, 25, 100, 0,  1, 0, 0, 75, 100, 1};
+    const GenParams P0W2E0{5, 25, 100, 0, -1, 0, 0, 75, 100, 1};
+    const GenParams P1W0E0{5, 25, 100, 1,  0, 0, 0, 75, 100, 1};
+    const GenParams P1W1E0{5, 25, 100, 1,  1, 0, 0, 75, 100, 1};
+    const GenParams P1W2E0{5, 25, 100, 1, -1, 0, 0, 75, 100, 1};
+    const GenParams P0W0E1{5, 25, 100, 0,  0, 1, 1, 75, 100, 1};
+    const GenParams P0W1E1{5, 25, 100, 0,  1, 1, 1, 75, 100, 1};
+    const GenParams P0W2E1{5, 25, 100, 0, -1, 1, 1, 75, 100, 1};
+    const GenParams P1W0E1{5, 25, 100, 1,  0, 1, 1, 75, 100, 1};
+    const GenParams P1W1E1{5, 25, 100, 1,  1, 1, 1, 75, 100, 1};
+    const GenParams P1W2E1{5, 25, 100, 1, -1, 1, 1, 75, 100, 1};
+
+private:
+
+};
 
 }
 
