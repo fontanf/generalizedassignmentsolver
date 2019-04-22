a1ab6005dcfdb7e5f64aa4ca1a2149bedb9f2016
Florian Fontan <dev@florian-fontan.fr>
2019-04-22 20:32:57 +0200

    Update LS

--- ub_lssimple/lssimple.cpp
+++ ub_lssimple/lssimple.cpp
@@ -9,81 +9,55 @@
 
 using namespace gap;
 
-bool move_shift(Solution& sol,
-        std::default_random_engine& gen,
-        std::uniform_int_distribution<>& dis_j,
-        std::uniform_int_distribution<>& dis_i,
-        Info& info)
+bool shift_swap(const Instance& ins, Solution& sol, Info& info)
 {
     (void)info;
-    ItemIdx j = dis_j(gen);
-    AgentIdx i_old = sol.agent(j);
-    AgentIdx i_new = sol.agent(j);
-    while (i_new == sol.agent(j))
-        i_new = dis_i(gen);
-    Value v = sol.value();
-    sol.set(j, i_new);
-    if (sol.feasible() > 0 || v < sol.value()) {
+    ItemIdx j_best = -1;
+    ItemIdx i_best = -1;
+    Value vshift_min = sol.value();
+    for (ItemIdx j=0; j<ins.item_number(); ++j) {
+        AgentIdx i_old = sol.agent(j);
+        for (AgentIdx i=0; i<ins.agent_number(); ++i) {
+            if (i == i_old)
+                continue;
+            sol.set(j, i);
+            if (sol.feasible() == 0 && vshift_min > sol.value()) {
+                j_best = j;
+                i_best = i;
+                vshift_min = sol.value();
+            }
+        }
         sol.set(j, i_old);
-        return false;
     }
-    return true;
-}
 
-bool move_swap(Solution& sol,
-        std::default_random_engine& gen,
-        std::uniform_int_distribution<>& dis_j,
-        Info& info)
-{
-    (void)info;
-    ItemIdx j1 = dis_j(gen);
-    ItemIdx j2 = j1;
-    AgentIdx i1 = sol.agent(j1);
-    AgentIdx i2 = sol.agent(j2);
-    while (i2 == i1) {
-        j2 = dis_j(gen);
-        i2 = sol.agent(j2);
-    }
-    Value v = sol.value();
-    sol.set(j1, i2);
-    sol.set(j2, i1);
-    if (sol.feasible() > 0 || v < sol.value()) {
+    ItemIdx j1_best = -1;
+    ItemIdx j2_best = -1;
+    Value vswap_min = sol.value();
+    for (ItemIdx j1=0; j1<ins.item_number(); ++j1) {
+        AgentIdx i1 = sol.agent(j1);
+        for (ItemIdx j2=j1+1; j2<ins.item_number(); ++j2) {
+            AgentIdx i2 = sol.agent(j2);
+            sol.set(j1, i2);
+            sol.set(j2, i1);
+            if (sol.feasible() == 0 && vswap_min > sol.value()) {
+                j1_best = j1;
+                j2_best = j2;
+                vswap_min = sol.value();
+            }
+            sol.set(j2, i2);
+        }
         sol.set(j1, i1);
-        sol.set(j2, i2);
-        return false;
     }
-    return true;
-}
 
-bool move_swap3(Solution& sol,
-        std::default_random_engine& gen,
-        std::uniform_int_distribution<>& dis_j,
-        Info& info)
-{
-    (void)info;
-    ItemIdx j1 = dis_j(gen);
-    ItemIdx j2 = j1;
-    ItemIdx j3 = j1;
-    AgentIdx i1 = sol.agent(j1);
-    AgentIdx i2 = sol.agent(j2);
-    AgentIdx i3 = sol.agent(j3);
-    while (i2 == i1) {
-        j2 = dis_j(gen);
-        i2 = sol.agent(j2);
-    }
-    while (i3 == i1 || i3 == i2) {
-        j3 = dis_j(gen);
-        i3 = sol.agent(j3);
-    }
-    Value v = sol.value();
-    sol.set(j1, i2);
-    sol.set(j2, i3);
-    sol.set(j3, i1);
-    if (sol.feasible() > 0 || v < sol.value()) {
-        sol.set(j1, i1);
-        sol.set(j2, i2);
-        sol.set(j3, i3);
+    if (j_best == -1 && j2_best == -1) { // No improvment
         return false;
+    } else if (j1_best == -1 || (j_best != -1 && vshift_min < vswap_min)) {
+        sol.set(j_best, i_best);
+    } else {
+        AgentIdx i1 = sol.agent(j1_best);
+        AgentIdx i2 = sol.agent(j2_best);
+        sol.set(j1_best, i2);
+        sol.set(j2_best, i1);
     }
     return true;
 }
@@ -93,8 +67,10 @@ bool move_gap(const Instance& ins, Solution& sol, AgentIdx m, ItemIdx n,
         Info& info)
 {
     (void)info;
-    std::random_shuffle(items.begin(), items.end());
-    std::random_shuffle(agents.begin(), agents.end());
+    if (n != ins.item_number())
+        std::random_shuffle(items.begin(), items.end());
+    if (m != ins.agent_number())
+        std::random_shuffle(agents.begin(), agents.end());
     std::vector<Weight> c(m, 0);
     for (AgentIdx i=0; i<m; ++i)
         c[i] = ins.capacity(agents[i]);
@@ -151,37 +127,42 @@ Solution gap::sol_lssimple(const Instance& ins, Solution& sol, Info info)
 
     Solution sol_best = sol;
     init_display(sol_best, lb, info);
-    std::default_random_engine gen(0);
-    std::uniform_int_distribution<> dis_j(0, ins.item_number() - 1);
-    std::uniform_int_distribution<> dis_i(0, ins.agent_number() - 1);
-    std::uniform_int_distribution<> dis_m(2, std::min(ins.agent_number() - 1, (AgentIdx)5));
-    for (Cpt it=1, it_without_improvment=0; info.check_time(); ++it, ++it_without_improvment) {
-        if (move_shift(sol, gen, dis_j, dis_i, info))
-            it_without_improvment = 0;
-        if (move_swap(sol, gen, dis_j, info))
-            it_without_improvment = 0;
-        if ((it + 1) % 10 == 0)
-            if (move_swap3(sol, gen, dis_j, info))
-                it_without_improvment = 0;
-        if ((it > 100000 && it % 10000 == 0) || (it > 1000000 && it % 1000 == 0))
-            if (move_gap(ins, sol, 2, ins.item_number(), items, agents, info))
-                it_without_improvment = 0;
-        if ((it > 1000000 && it % 100000 == 0) || (it > 10000000 && it % 10000 == 0))
-            if (move_gap(ins, sol, 3, ins.item_number(), items, agents, info))
-                it_without_improvment = 0;
-        if ((it > 10000000 && it % 1000000 == 0) || (it > 100000000 && it % 100000 == 0))
-            if (move_gap(ins, sol, 4, 1024, items, agents, info))
-                it_without_improvment = 0;
-        if ((it > 100000000 && it % 10000000 == 0) || (it > 1000000000 && it % 1000000 == 0))
-            if (move_gap(ins, sol, 5, 512, items, agents, info))
-                it_without_improvment = 0;
+    for (Cpt it=0; info.check_time(); ++it) {
+        while (shift_swap(ins, sol, info)) {  }
+        std::stringstream ss;
+        ss << "it " << it << " shift-swap";
+        sol_best.update(sol, lb, ss, info);
 
-        if (it % 100000 == 0 && sol_best.value() > sol.value()) {
-            it_without_improvment = 0;
-            std::stringstream ss;
-            ss << "it " << it;
-            sol_best.update(sol, lb, ss, info);
+        bool b = false;
+        for (ItemIdx k=0; ; ++k) {
+            ItemIdx n = 2;
+            if (k <= 10) {
+                n = 2;
+            } else if (k <= 50) {
+                n = (k % 2) + 2;
+            } else if (k <= 200) {
+                n = (k % 3) + 2;
+            } else if (k <= 1000) {
+                n = (k % 4) + 2;
+            } else if (k <= 5000) {
+                n = (k % 5) + 2;
+            } else if (k <= 20000) {
+                n = (k % 6) + 2;
+            } else {
+                n = (k % 7) + 2;
+            }
+            if (n > ins.agent_number())
+                n = ins.agent_number();
+            if (move_gap(ins, sol, n, ins.item_number(), items, agents, info)) {
+                std::stringstream ss;
+                ss << "it " << it << " gap " << n;
+                sol_best.update(sol, lb, ss, info);
+                b = true;
+                break;
+            }
         }
+        if (b)
+            continue;
     }
     return algorithm_end(sol, info);
 }
