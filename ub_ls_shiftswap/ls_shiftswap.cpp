11d8ccd5432eff85b02746ac91300ed5c5389f9b
Florian Fontan <dev@florian-fontan.fr>
2019-05-03 12:26:38 +0200

    Update Tabu search

--- ub_ls_shiftswap/ls_shiftswap.cpp
+++ ub_ls_shiftswap/ls_shiftswap.cpp
@@ -232,90 +232,115 @@ bool gap::tripleswap_best(const Instance& ins, Solution& sol, Info& info)
 
 /******************************************************************************/
 
-void ts_shiftswap_move(const Instance& ins, Solution& sol,
-        std::vector<std::vector<Cpt>>& tabu, Cpt tabu_size, Cpt it)
-{
-    Value v_best = -1;
-    ItemIdx j_best = -1;
-    AgentIdx i_best = -1;
-    for (ItemIdx j=0; j<ins.item_number(); ++j) {
-        AgentIdx i_old = sol.agent(j);
-        for (AgentIdx i=0; i<ins.agent_number(); ++i) {
-            if (i == i_old)
-                continue;
-            if (it - tabu[j][i] < tabu_size)
-                continue;
-            sol.set(j, i);
-            if (sol.feasible() == 0 && (v_best == -1 || v_best > sol.value())) {
-                v_best = sol.value();
-                j_best = j;
-                i_best = i;
-            }
-        }
-        sol.set(j, i_old);
-    }
-
-    ItemIdx j1_best = -1;
-    ItemIdx j2_best = -1;
-    for (ItemIdx j1=0; j1<ins.item_number(); ++j1) {
-        AgentIdx i1 = sol.agent(j1);
-        for (ItemIdx j2=j1+1; j2<ins.item_number(); ++j2) {
-            AgentIdx i2 = sol.agent(j2);
-            if (i2 == i1)
-                continue;
-            if (it - tabu[j1][i2] < tabu_size)
-                continue;
-            if (it - tabu[j2][i1] < tabu_size)
-                continue;
-            sol.set(j1, i2);
-            sol.set(j2, i1);
-            if (sol.feasible() == 0 && (v_best == -1 || v_best > sol.value())) {
-                v_best = sol.value();
-                j_best = -1;
-                i_best = -1;
-                j1_best = j1;
-                j2_best = j2;
-            }
-            sol.set(j2, i2);
-        }
-        sol.set(j1, i1);
-    }
-
-    if (v_best == -1) {
-        std::cout << "toto" << std::endl;
-        return;
-    }
-
-    if (j1_best != -1) {
-        AgentIdx i1 = sol.agent(j1_best);
-        AgentIdx i2 = sol.agent(j2_best);
-        if (v_best >= sol.value()) {
-            tabu[j1_best][i1] = it;
-            tabu[j2_best][i2] = it;
-        }
-        sol.set(j1_best, i2);
-        sol.set(j2_best, i1);
-    } else {
-        if (v_best >= sol.value())
-            tabu[j_best][sol.agent(j_best)] = it;
-        sol.set(j_best, i_best);
-    }
-}
-
 Solution gap::sol_ts_shiftswap(const Instance& ins, Solution& sol, std::default_random_engine& gen, Info info)
 {
     if (!sol.is_complete() || sol.feasible() > 0)
         sol = sol_random(ins, gen);
     Solution sol_best = sol;
     init_display(sol_best, 0, info);
+
+    Cpt neig_size = 10000;
     Cpt tabu_size = ins.item_number() / 20;
+
+    AgentIdx m = ins.agent_number();
+    ItemIdx n = ins.item_number();
+    Cpt neigh_totalsize = n * m + (n * (n + 1)) / 2;
     std::vector<std::vector<Cpt>> tabu(ins.item_number(), std::vector<Cpt>(ins.agent_number(), - tabu_size - 1));
+    std::uniform_int_distribution<Cpt> dis_ss(1, neigh_totalsize);
+    std::uniform_int_distribution<ItemIdx> dis_j(0, n - 1);
+    std::uniform_int_distribution<ItemIdx> dis_j2(0, n - 2);
+    std::uniform_int_distribution<AgentIdx> dis_i1(0, m - 2);
+    std::uniform_int_distribution<AgentIdx> dis_i2(0, m - 3);
+    std::uniform_real_distribution<double> dis(0, 1);
 
     for (Cpt it=0; info.check_time(); ++it) {
-        ts_shiftswap_move(ins, sol, tabu, tabu_size, it);
+        if (neig_size < neigh_totalsize / 2)
+            neig_size++;
+
+        Value v_best = -1;
+        ItemIdx j_best = -1;
+        AgentIdx i_best = -1;
+        ItemIdx j1_best = -1;
+        ItemIdx j2_best = -1;
+
+        for (Cpt it2=0; it2<neig_size;) {
+            Cpt p = dis_ss(gen);
+            if (p <= m * n) { // shift
+                ItemIdx j = dis_j(gen);
+                AgentIdx i_old = sol.agent(j);
+                AgentIdx i = dis_i1(gen);
+                if (i >= i_old)
+                    i++;
+
+                if (it - tabu[j][i] < tabu_size)
+                    continue;
+
+                sol.set(j, i);
+                Value v = sol.value();
+                if (sol.feasible() == 0 && (v_best == -1 || v_best > v)) {
+                    v_best = v;
+                    j_best = j;
+                    i_best = i;
+                    j1_best = -1;
+                    j2_best = -1;
+                }
+                sol.set(j, i_old);
+            } else { // swap
+                ItemIdx j1 = dis_j(gen);
+                AgentIdx i1 = sol.agent(j1);
+
+                ItemIdx j2 = dis_j2(gen);
+                if (j2 >= j1)
+                    j2++;
+                AgentIdx i2 = sol.agent(j2);
+
+                if (i1 == i2)
+                    continue;
+                if (it - tabu[j1][i2] < tabu_size)
+                    continue;
+                if (it - tabu[j2][i1] < tabu_size)
+                    continue;
+
+                sol.set(j1, i2);
+                sol.set(j2, i1);
+                Value v = sol.value();
+                if (sol.feasible() == 0 && (v_best == -1 || v_best > v)) {
+                    v_best = v;
+                    j_best = -1;
+                    i_best = -1;
+                    j1_best = j1;
+                    j2_best = j2;
+                }
+                sol.set(j1, i1);
+                sol.set(j2, i2);
+            }
+
+            ++it2;
+        }
+
+        if (v_best == -1) {
+            std::cout << "toto" << std::endl;
+            continue;
+        }
+
+        if (j1_best != -1) {
+            AgentIdx i1 = sol.agent(j1_best);
+            AgentIdx i2 = sol.agent(j2_best);
+            if (v_best >= sol.value()) {
+                tabu[j1_best][i1] = it;
+                tabu[j2_best][i2] = it;
+            }
+            sol.set(j1_best, i2);
+            sol.set(j2_best, i1);
+        } else {
+            if (v_best >= sol.value())
+                tabu[j_best][sol.agent(j_best)] = it;
+            sol.set(j_best, i_best);
+        }
+
+        // Update best solution
         if (sol_best.value() > sol.value()) {
             std::stringstream ss;
-            ss << "it " << it;
             sol_best.update(sol, 0, ss, info);
         }
     }
@@ -353,7 +378,7 @@ Solution gap::sol_sa_shiftswap(const Instance& ins, Solution& sol, std::default_
     Cpt it_without_change = 0;
 
     for (double t=t0; info.check_time(); t*=alpha) {
-        for (Cpt it=0; it<l; ++it) {
+        for (Cpt it=0; it<l;) {
             Value v = sol.value();
             Cpt p = dis_ss(gen);
             if (p <= m * n) { // shift
@@ -370,11 +395,14 @@ Solution gap::sol_sa_shiftswap(const Instance& ins, Solution& sol, std::default_
                 }
             } else { // swap
                 ItemIdx j1 = dis_j(gen);
-                ItemIdx j2 = dis_j2(gen);
-                if (j2 >= j1)
-                    j2++;
                 AgentIdx i1 = sol.agent(j1);
+
+                ItemIdx j2 = dis_j2(gen);
                 AgentIdx i2 = sol.agent(j2);
+
+                if (i1 == i2)
+                    continue;
+
                 sol.set(j1, i2);
                 sol.set(j2, i1);
                 if (sol.feasible() > 0
@@ -399,6 +427,8 @@ Solution gap::sol_sa_shiftswap(const Instance& ins, Solution& sol, std::default_
                 std::stringstream ss;
                 sol_best.update(sol, 0, ss, info);
             }
+
+            ++it;
         }
     }
     return algorithm_end(sol_best, info);
