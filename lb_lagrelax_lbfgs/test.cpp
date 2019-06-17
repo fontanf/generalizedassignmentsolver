9d314afd14037fe077d859bdefc08f7dd64f158c
Florian Fontan <dev@florian-fontan.fr>
2019-06-17 09:20:31 +0200

    Minor fixes

--- lb_lagrelax_lbfgs/test.cpp
+++ lb_lagrelax_lbfgs/test.cpp
@@ -1,6 +1,6 @@
 #include "gap/lib/tester.hpp"
 #include "gap/lib/generator.hpp"
-#include "gap/lb_lagrelax_bundle/lagrelax_bundle.hpp"
+#include "gap/lb_lagrelax_lbfgs/lagrelax_lbfgs.hpp"
 
 using namespace gap;
 
