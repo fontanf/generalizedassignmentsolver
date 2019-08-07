bae71c1bafd0267703663730231d718b4e7f9d61
Florian Fontan <dev@florian-fontan.fr>
2019-08-07 11:40:36 +0200

    Update bounds

--- lib/checker.cpp
+++ lib/checker.cpp
@@ -1,6 +1,8 @@
 #include "gap/lib/instance.hpp"
 #include "gap/lib/solution.hpp"
 
+#include <iomanip>
+
 using namespace gap;
 
 int main(int argc, char *argv[])
@@ -14,11 +16,28 @@ int main(int argc, char *argv[])
     Instance ins(instancefile);
 
     Solution sol(ins, certfile);
+    Cost lb = 0;
+    std::ifstream f_bound(instancefile + ".bound");
+    if (f_bound.good())
+        f_bound >> lb;
+    f_bound.close();
 
     if (!sol.feasible()) {
         std::cout << "INFEASIBLE" << std::endl;
     } else {
-        std::cout << sol.cost() << std::endl;
+        std::cout << std::left << std::setw(12) << "UB";
+        std::cout << std::left << std::setw(12) << "LB";
+        std::cout << std::left << std::setw(10) << "GAP";
+        std::cout << std::left << std::setw(10) << "GAP (%)";
+        std::cout << std::endl;
+
+        double gap = (lb == 0)? std::numeric_limits<double>::infinity():
+            (double)(10000 * (sol.cost() - lb) / lb) / 100;
+        std::cout << std::left << std::setw(12) << sol.cost();
+        std::cout << std::left << std::setw(12) << lb;
+        std::cout << std::left << std::setw(10) << sol.cost() - lb;
+        std::cout << std::left << std::setw(10) << gap;
+        std::cout << std::endl;
     }
     return 0;
 }
