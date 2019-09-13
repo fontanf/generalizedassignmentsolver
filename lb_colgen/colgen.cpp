c8285ee034c7a4c45f2228f314d20d3f9e7413fc
Florian Fontan <dev@florian-fontan.fr>
2019-09-13 16:35:39 +0200

    Continue colgen

--- lb_colgen/colgen.cpp
+++ lb_colgen/colgen.cpp
@@ -22,5 +22,36 @@ void gap::lb_colgen(ColGenData d)
 {
     VER(d.info, "*** colgen s " << d.solver << " ***" << std::endl);
 
+    if (d.solver == "clp") {
+#ifndef COINOR_FOUND
+        std::cerr << "\033[31m" << "ERROR, unable to use solver \"clp\"" << "\033[0m" << std::endl;
+        return;
+#endif
+    } else if (d.solver == "cplex") {
+#ifndef CPLEX_FOUND
+        std::cerr << "\033[31m" << "ERROR, unable to use solver \"cplex\"" << "\033[0m" << std::endl;
+        return;
+#endif
+    } else if (d.solver == "gurobi") {
+#ifndef GUROBI_FOUND
+        std::cerr << "\033[31m" << "ERROR, unable to use solver \"gurobi\"" << "\033[0m" << std::endl;
+        return;
+#endif
+    }
+
+    // Add initial columns
+    if (d.columns.empty()) {
+
+    } else {
+
+    }
+
+    for (;;) {
+        // Solve LP
+
+        // Find and add new columns
+
+    }
+
 }
 
