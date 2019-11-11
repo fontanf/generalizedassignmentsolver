ad495bfa5123082d2f158167fb735961e0a40df5
Florian Fontan <dev@florian-fontan.fr>
2019-11-11 18:43:27 +0100

    Fix .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -2,7 +2,7 @@ def Settings( **kwargs ):
     return {
             'flags': [
                 '-x', 'c++',
-                '-Wall', '-Wextra', '-Werror'
+                '-Wall', '-Wextra', '-Werror',
                 '-DCOINOR_FOUND',
                 '-DDLIB_FOUND',
                 '-DCPLEX_FOUND',
