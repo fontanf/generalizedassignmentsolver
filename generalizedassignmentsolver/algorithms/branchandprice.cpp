7a60b567566a75e26a1564dd04ad23c240a42ed9
Florian Fontan <dev@florian-fontan.fr>
2020-07-05 12:24:23 +0200

    Improve branch-and-price

--- generalizedassignmentsolver/algorithms/branchandprice.cpp
+++ generalizedassignmentsolver/algorithms/branchandprice.cpp
@@ -19,8 +19,8 @@ BranchAndPriceOutput& BranchAndPriceOutput::algorithm_end(Info& info)
 struct BranchAndPriceNode
 {
     std::shared_ptr<BranchAndPriceNode> father = nullptr;
-    ItemIdx j = -1;
-    AgentIdx i = -1;
+    AltIdx k = -1;
+    int8_t v = 0;
     double value = 0;
 };
 
@@ -72,18 +72,31 @@ BranchAndPriceOutput generalizedassignmentsolver::branchandprice(
         Solution solution(instance);
         std::fill(fixed_alternatives.begin(), fixed_alternatives.end(), -1);
         auto node_tmp = node;
+        AltIdx depth = 0;
         while (node_tmp->father != nullptr) {
-            solution.set(node_tmp->j, node_tmp->i);
-            for (AgentIdx i = 0; i < instance.agent_number(); ++i)
-                fixed_alternatives[instance.alternative_index(node_tmp->j, i)] = (i == node_tmp->i);
+            if (node_tmp->v == 1) {
+                auto a = instance.alternative(node_tmp->k);
+                solution.set(node_tmp->k);
+                for (AgentIdx i = 0; i < instance.agent_number(); ++i)
+                    fixed_alternatives[instance.alternative_index(a.j, i)] = 0;
+            }
+            fixed_alternatives[node_tmp->k] = node_tmp->v;
             node_tmp = node_tmp->father;
+            depth++;
         }
+
+        // Run column generation
         auto colgen_output = columngeneration(instance, colgen_parameters);
         if (parameters.tree_search_algorithm == "bfs") {
             std::stringstream ss;
             ss << "node " << output.node_number;
             output.update_lower_bound(colgen_output.lower_bound, ss, parameters.info);
         }
+        //std::cout << "node_number " << output.node_number
+            //<< " n " << solution.item_number()
+            //<< " c " << solution.cost()
+            //<< " lb " << colgen_output.lower_bound
+            //<< std::endl;
 
         // Check bound
         if (output.solution.feasible()
@@ -91,75 +104,82 @@ BranchAndPriceOutput generalizedassignmentsolver::branchandprice(
             continue;
 
         // Compute next variable to branch on.
-        ItemIdx j_best = -1;
         AltIdx k_best = -1;
+        AgentIdx i_best = -1;
         if (parameters.branching_rule == "most-fractional") {
             for (ItemIdx j = 0; j < instance.item_number(); ++j) {
-                if (solution.agent(j) != -1)
-                    continue;
                 for (AgentIdx i = 0; i < instance.agent_number(); ++i) {
                     AltIdx k = instance.alternative_index(j, i);
+                    if (fixed_alternatives[k] != -1)
+                        continue;
                     if (k_best == -1
                             || std::abs(colgen_output.x[k_best] - 0.5)
                             < std::abs(colgen_output.x[k] - 0.5)) {
                         k_best = k;
-                        j_best = j;
+                        i_best = i;
                     }
                 }
             }
         } else {
             for (ItemIdx j = 0; j < instance.item_number(); ++j) {
-                if (solution.agent(j) != -1)
-                    continue;
                 for (AgentIdx i = 0; i < instance.agent_number(); ++i) {
                     AltIdx k = instance.alternative_index(j, i);
+                    if (fixed_alternatives[k] != -1)
+                        continue;
                     if (k_best == -1
                             || colgen_output.x[k_best] < colgen_output.x[k]) {
                         k_best = k;
-                        j_best = j;
+                        i_best = i;
                     }
                 }
             }
         }
 
-        for (AgentIdx i = 0; i < instance.agent_number(); ++i)
-            fixed_alternatives[instance.alternative_index(j_best, i)] = 0;
+        if (solution.remaining_capacity(i_best) < instance.alternative(k_best).w)
+            continue;
 
-        // Children
-        for (AgentIdx i = 0; i < instance.agent_number(); ++i) {
-            AltIdx k = instance.alternative_index(j_best, i);
-
-            if (solution.remaining_capacity(i) < instance.alternative(k).w)
-                continue;
-
-            // Update solution
-            solution.set(k);
-            if (solution.feasible()) {
-                std::stringstream ss;
-                ss << "node " << output.node_number;
-                output.update_solution(solution, ss, parameters.info);
-                if (output.optimal())
-                    return output.algorithm_end(parameters.info);
-                continue;
-            }
-
-            auto child = std::make_shared<BranchAndPriceNode>();
-            child->father = node;
-            child->i = i;
-            child->j = j_best;
-            if (parameters.tree_search_algorithm == "dfs") {
-                child->value = - solution.item_number() - colgen_output.x[k];
-            } else if (parameters.tree_search_algorithm == "lds") {
-                child->value = node->value + colgen_output.x[k_best] - colgen_output.x[k];
-            } else {
-                fixed_alternatives[k] = 1;
-                auto colgen_output_child = columngeneration(instance, colgen_parameters);
-                fixed_alternatives[k] = 0;
-                child->value = (double)colgen_output_child.lower_bound - (double)solution.item_number() / instance.item_number();
-            }
-
-            nodes.insert(child);
+        // Update solution
+        solution.set(k_best);
+        if (solution.feasible()) {
+            std::stringstream ss;
+            ss << "node " << output.node_number;
+            output.update_solution(solution, ss, parameters.info);
+            if (output.optimal())
+                return output.algorithm_end(parameters.info);
+            continue;
         }
+
+        // Child 1
+        auto child_1 = std::make_shared<BranchAndPriceNode>();
+        child_1->father = node;
+        child_1->k = k_best;
+        child_1->v = 1;
+        if (parameters.tree_search_algorithm == "dfs") {
+            child_1->value = - 0.5 - depth;
+        } else if (parameters.tree_search_algorithm == "lds") {
+            child_1->value = node->value;
+        } else {
+            fixed_alternatives[k_best] = 1;
+            auto colgen_output_child = columngeneration(instance, colgen_parameters);
+            child_1->value = (double)colgen_output_child.lower_bound - (double)(solution.item_number() + 1) / instance.item_number();
+        }
+        nodes.insert(child_1);
+
+        // Child 0
+        auto child_0 = std::make_shared<BranchAndPriceNode>();
+        child_0->father = node;
+        child_0->k = k_best;
+        child_0->v = 0;
+        if (parameters.tree_search_algorithm == "dfs") {
+            child_0->value = - depth;
+        } else if (parameters.tree_search_algorithm == "lds") {
+            child_0->value = node->value + colgen_output.x[k_best];
+        } else {
+            fixed_alternatives[k_best] = 0;
+            auto colgen_output_child = columngeneration(instance, colgen_parameters);
+            child_0->value = (double)colgen_output_child.lower_bound - (double)solution.item_number() / instance.item_number();
+        }
+        nodes.insert(child_0);
     }
 
     return output.algorithm_end(parameters.info);
