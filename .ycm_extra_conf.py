e6604d549e11a40c523a38cb7cbf99a6dad8ee2c
Florian Fontan <dev@florian-fontan.fr>
2023-01-08 11:54:54 +0100

    Update WORKSPACE

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -3,28 +3,64 @@ def Settings(**kwargs):
             'flags': [
                 '-x', 'c++',
                 '-Wall', '-Wextra', '-Werror',
-                '-DCOINOR_FOUND',
-                '-DCPLEX_FOUND',
-                '-DGUROBI_FOUND',
-                '-DGECODE_FOUND',
-                '-DKNITRO_FOUND',
-                '-DIL_STD',  # Cplex
                 '-I', '.',
+
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                'json/single_include/',
+
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                'googletest/googletest/include/',
+
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                'boost/',
+
+                # optimizationtools
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                # '-I', './../'
+                'optimizationtools/',
+
+                # localsearchsolver
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                # '-I', './../',
+                'localsearchsolver/',
+
+                # columngenerationsolver
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                # '-I', './../',
+                'columngenerationsolver/',
+
+                # knapsacksolver
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                # '-I', './../',
+                'knapsacksolver/',
+
+                # dlib
+                '-I', './bazel-generalizedassignmentsolver/external/'
+                'dlib/dlib-19.19/',
+
+                # COINOR
+                '-DCOINOR_FOUND',
                 '-I', '/home/florian/Programmes/coinbrew/dist/include/',
+
+                # Gurobi
+                '-DGUROBI_FOUND',
+                '-I', '/home/florian/Programmes/gurobi811/linux64/include/',
+
+                # CPLEX
+                '-DCPLEX_FOUND',
+                '-DIL_STD',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/concert/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cplex/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cpoptimizer/include/',
-                '-I', '/home/florian/Programmes/gurobi811/linux64/include/',
-                '-I', '/home/florian/Programmes/knitro-13.1.0-z-Linux-Intel17-64/include/',
+
+                # Gecode
+                '-DGECODE_FOUND',
                 '-I', '/home/florian/Programmes/gecode-release-6.2.0/',
-                '-I', './bazel-generalizedassignmentsolver/external/json/single_include/',
-                '-I', './bazel-generalizedassignmentsolver/external/googletest/googletest/include/',
-                '-I', './bazel-generalizedassignmentsolver/external/boost/',
-                '-I', './bazel-generalizedassignmentsolver/external/optimizationtools/',
-                '-I', './bazel-generalizedassignmentsolver/external/localsearchsolver/',
-                '-I', './bazel-generalizedassignmentsolver/external/columngenerationsolver/',
-                # '-I', './../columngenerationsolver/',
-                '-I', './bazel-generalizedassignmentsolver/external/knapsacksolver/',
-                '-I', './bazel-generalizedassignmentsolver/external/dlib/dlib-19.19/',
+
+                # Knitro
+                '-DKNITRO_FOUND',
+                '-I', '/home/florian/Programmes/'
+                'knitro-13.1.0-z-Linux-Intel17-64/include/',
+
                 ],
             }
